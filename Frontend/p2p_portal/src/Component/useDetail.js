3cdbb0f1b9ede9788e81d8ba65e6c4c71b2fabb7
Prem R Pattanayak <prem.p.in@rupeek.com>
2023-08-04 11:11:02 +0530

    Beautify

--- Frontend/p2p_portal/src/Component/useDetail.js
+++ Frontend/p2p_portal/src/Component/useDetail.js
@@ -1,33 +1,38 @@
-import React,{useState} from 'react'
-import axios from "axios"
-import {Link} from 'react-router-dom';
+import React, { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 const UserDetails = () => {
-    const [name, setname] = useState("");
-    const [number, setnumber] = useState("");
-    const [creditScore, setcreditScore] = useState("");
-    const [AadhaarNumber, setAadhaarNumber] = useState("");
-    const [kyc, setkyc] = useState("");
-    const [gold,setgold]=useState("")
-    const handleAdd=(e)=>{
-        e.preventDefault()
-        try {
-            axios.post("api",{
-                name,number,creditScore,AadhaarNumber,kyc,gold
-            })
-            
-        } catch (error) {
-            console.log(error,"err")
-            
-        }
-
+  const [name, setname] = useState("");
+  const [number, setnumber] = useState("");
+  const [creditScore, setcreditScore] = useState("");
+  const [AadhaarNumber, setAadhaarNumber] = useState("");
+  const [kyc, setkyc] = useState("");
+  const [gold, setgold] = useState("");
+  const handleAdd = (e) => {
+    e.preventDefault();
+    try {
+      axios.post("api", {
+        name,
+        number,
+        creditScore,
+        AadhaarNumber,
+        kyc,
+        gold,
+      });
+    } catch (error) {
+      console.log(error, "err");
     }
+  };
   return (
     <div className="bg-gray-100 h-screen flex justify-center items-center">
       <div className="max-w-md w-full p-6 bg-white rounded-md shadow-md">
-        <h2 className="text-2xl font-semibold mb-4 text-center">User Details</h2>
+        <h2 className="text-2xl font-semibold mb-4 text-center">
+          User Details
+        </h2>
         <form>
           <div className="mb-4">
-            <label htmlFor="name"
+            <label
+              htmlFor="name"
               className="block text-gray-700 text-sm font-bold mb-2"
             >
               Name
@@ -91,7 +96,7 @@ const UserDetails = () => {
               htmlFor="gold"
               className="block text-gray-700 text-sm font-bold mb-2"
             >
-             Gold Weight (in Grams) 
+              Gold Weight (in Grams)
             </label>
             <input
               type="text"
@@ -100,34 +105,38 @@ const UserDetails = () => {
               onChange={(e) => setgold(e.target.value)}
               placeholder="Gold Weight"
             />
-          
           </div>
           <div className="mb-6">
             <label
               htmlFor="kyc"
               className="block text-gray-700 text-sm font-bold mb-2"
             >
-             KYC
+              KYC
             </label>
-           <select
-           className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-
-            name="kyc" onSelect={(e)=>setkyc(e.target.value)} id="educationType">
-            <option value="yes">YES</option>
-            <option value="no">NO</option>
-           
-           </select>
+            <select
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+              name="kyc"
+              onSelect={(e) => setkyc(e.target.value)}
+              id="educationType"
+            >
+              <option value="yes">YES</option>
+              <option value="no">NO</option>
+            </select>
           </div>
-        
+
           <div className="flex items-center justify-between">
-            <button type="button" onClick={handleAdd} className="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-md focus:outline-none">
-                Submit
+            <button
+              type="button"
+              onClick={handleAdd}
+              className="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-md focus:outline-none"
+            >
+              Submit
             </button>
           </div>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default UserDetails
+export default UserDetails;
